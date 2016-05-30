dba9b127ebda1723049a5ab2334d1ab94d3f7f58
Dorjee Dhondup <dhondo01@luther.edu>
2016-05-30 17:29:20 -0500

    Test hello java program

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -4,5 +4,8 @@
 public class Hello {
     public static void main (String[] args) {
         System.out.println("Hello, Dorjee!");
+
+        int myFirstNumber = 15%8; // 7
+        System.out.println("My lucky number is " + myFirstNumber);
     }
 }
