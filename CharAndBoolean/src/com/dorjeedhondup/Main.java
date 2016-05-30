496cc8eea7791632b4d7c3503ef4fc68df1fb74b
Dorjee Dhondup <dhondo01@luther.edu>
2016-05-30 18:03:43 -0500

    Add more primitive type float and double test programs

---
+++ CharAndBoolean/src/com/dorjeedhondup/Main.java
@@ -0,0 +1,11 @@
+package com.dorjeedhondup;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char myChar = '\u0F00'; // single quotes // unicode for copyright is '\u00A9'
+        System.out.println(myChar);
+        boolean found = false;
+
+    }
+}
