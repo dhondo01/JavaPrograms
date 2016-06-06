f3600fe0de956aa30e782173f7cb5c46e2bb483d
Dorjee Dhondup <dhondo01@luther.edu>
2016-06-06 10:14:22 -0500

    Add string examples

---
+++ Strings/src/com/dorjeedhondup/Main.java
@@ -0,0 +1,11 @@
+package com.dorjeedhondup;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String firstName = "Dorjee"; // double quotes
+        String lastName = "Dhondup";
+        String fullName = firstName + " " + lastName;
+        System.out.println("I'm "+ fullName);
+    }
+}
